d4b5efb3af41305f8371c3bc9033bef16818577e
Dario Sosa <dsosa.ar@gmail.com>
2024-07-27 16:45:59 -0300

    agrego la funcion sumar a ejemplo1 y la funcion restar a ejemplo 2

--- ejemplo1.py
+++ ejemplo1.py
@@ -2,3 +2,8 @@ def mensaje():
     print("Estoy trabajando con GIT")
     
 mensaje()
+
+def sumar(n1,n2):
+    print("El resultado de la suma es: ", n1 + n2)
+    
+sumar(int(input("Ingrese el primer numero: ")),int(input("Ingrese el segundo numero: ")))
---
+++ ejemplo2.py
@@ -0,0 +1,4 @@
+def restar(n1,n2):
+    print("El resultado de la resta es: ", n1 - n2)
+    
+restar(int(input("Ingrese el primer numero: ")),int(input("Ingrese el segundo numero: ")))
