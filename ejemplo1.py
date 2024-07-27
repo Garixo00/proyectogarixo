fab7575ccc30267624d7d189b4b09ea4a1d4f02f
Dario Sosa <dsosa.ar@gmail.com>
2024-07-27 17:11:17 -0300

    agrego en ejemplo1 la multiplicacion y a ejemplo2 la division

--- ejemplo1.py
+++ ejemplo1.py
@@ -7,3 +7,8 @@ def sumar(n1,n2):
     print("El resultado de la suma es: ", n1 + n2)
     
 sumar(int(input("Ingrese el primer numero: ")),int(input("Ingrese el segundo numero: ")))
+
+def multiplicar(n1,n2):
+    print("El resultado de la multiplicacion es: ", n1 * n2)
+    
+multiplicar(int(input("Ingrese el primer numero: ")),int(input("Ingrese el segundo numero: ")))
--- ejemplo2.py
+++ ejemplo2.py
@@ -2,3 +2,8 @@ def restar(n1,n2):
     print("El resultado de la resta es: ", n1 - n2)
     
 restar(int(input("Ingrese el primer numero: ")),int(input("Ingrese el segundo numero: ")))
+
+def division(n1,n2):
+    print("El resultado de la division es: ", n1 / n2)
+    
+division(int(input("Ingrese el primer numero: ")),int(input("Ingrese el segundo numero: ")))
